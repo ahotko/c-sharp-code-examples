9529430c4e267b84f69a048a7c12466f52235479
ahotko <ales.hotko@zejn.si>
2020-05-29 08:23:27 +0200

    Added Span<T> Snippet (not complete yet)

---
+++ CSharp Code Samples/CodeSamples/UsefulClasses/SpanSample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CodeSamples.UsefulClasses
+{
+    public class SpanSample : SampleExecute
+    {
+        private void SpanOverArray()
+        {
+            //var arrayOfInt = new int[100];
+            //var span = new Span<int>(arrayOfInt);
+
+            //byte data = 0;
+            //for (int ctr = 0; ctr < arraySpan.Length; ctr++)
+            //    arraySpan[ctr] = data++;
+        }
+
+        private void SpanOverNativeMemory()
+        {
+            //var native = Marshal.AllocHGlobal(100);
+            //Span<byte> nativeSpan;
+            //unsafe
+            //{
+            //    nativeSpan = new Span<byte>(native.ToPointer(), 100);
+            //}
+        }
+
+        public override void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
