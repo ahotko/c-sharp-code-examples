60675d4e8c59124f7d7e5d86a936726babe560eb
Ales Hotko <ales.hotko@tompit.net>
2020-06-23 16:25:41 +0200

    Update of Struct Snippet

--- CSharp Code Samples/CodeSamples/Structures/StructSample.cs
+++ CSharp Code Samples/CodeSamples/Structures/StructSample.cs
@@ -103,6 +103,33 @@ namespace CodeSamples.Structures
             public DosFileAttributes Attributes;
         }
 
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct BoolToByte
+        {
+            [FieldOffset(0)]
+            public bool BoolProperty;
+
+            [FieldOffset(0)]
+            public byte ByteProperty;
+        }
+
+        private void PrimitiveTypesSizes()
+        {
+            Console.WriteLine($"Size of 'sbyte' = {sizeof(sbyte)} bytes ({sizeof(sbyte) * 8} bits)");
+            Console.WriteLine($"Size of 'byte' = {sizeof(byte)} bytes ({sizeof(byte) * 8} bits)");
+            Console.WriteLine($"Size of 'short' = {sizeof(short)} bytes ({sizeof(short) * 8} bits)");
+            Console.WriteLine($"Size of 'ushort' = {sizeof(ushort)} bytes ({sizeof(ushort) * 8} bits)");
+            Console.WriteLine($"Size of 'int' = {sizeof(int)} bytes ({sizeof(int) * 8} bits)");
+            Console.WriteLine($"Size of 'uint' = {sizeof(uint)} bytes ({sizeof(uint) * 8} bits)");
+            Console.WriteLine($"Size of 'long' = {sizeof(long)} bytes ({sizeof(long) * 8} bits)");
+            Console.WriteLine($"Size of 'ulong' = {sizeof(ulong)} bytes ({sizeof(ulong) * 8} bits)");
+            Console.WriteLine($"Size of 'char' = {sizeof(char)} bytes ({sizeof(char) * 8} bits)");
+            Console.WriteLine($"Size of 'float' = {sizeof(float)} bytes ({sizeof(float) * 8} bits)");
+            Console.WriteLine($"Size of 'double' = {sizeof(double)} bytes ({sizeof(double) * 8} bits)");
+            Console.WriteLine($"Size of 'decimal' = {sizeof(decimal)} bytes ({sizeof(decimal) * 8} bits)");
+            Console.WriteLine($"Size of 'bool' = {sizeof(bool)} bytes ({sizeof(bool) * 8} bits)");
+        }
+
         public override void Execute()
         {
             Title("StructSampleExecute");
@@ -117,16 +144,16 @@ namespace CodeSamples.Structures
             Section("Convert float to uint");
             var floatAndUint = new FloatAndUint();
             floatAndUint.FloatValue = 3.1415926f;
-            Console.WriteLine($"We have an float ({floatAndUint.FloatValue}) we can show as uint (in hex) = (0x{floatAndUint.UintValue:X8}).");
+            Console.WriteLine($"We have a float ({floatAndUint.FloatValue}) we can show as uint (in hex) = (0x{floatAndUint.UintValue:X8}).");
             floatAndUint.UintValue = 0x40490fda;  //PI in IEEE-754 format
             Console.WriteLine($"We have an uint (in hex) = (0x{floatAndUint.UintValue:X8}) we can show as float = ({floatAndUint.FloatValue}).");
 
             Section("Convert double to ulong");
             var doubleAndUlong = new DoubleAndUlong();
             doubleAndUlong.DoubleValue = Math.E;
-            Console.WriteLine($"We have an float ({doubleAndUlong.DoubleValue}) we can show as uint (in hex) = (0x{doubleAndUlong.UlongValue:X16}).");
+            Console.WriteLine($"We have a double ({doubleAndUlong.DoubleValue}) we can show as ulong (in hex) = (0x{doubleAndUlong.UlongValue:X16}).");
             doubleAndUlong.UlongValue = 0x400921fb54442d18;  //PI in IEEE-754 format
-            Console.WriteLine($"We have an uint (in hex) = (0x{doubleAndUlong.UlongValue:X16}) we can show as float = ({doubleAndUlong.DoubleValue}).");
+            Console.WriteLine($"We have an ulong (in hex) = (0x{doubleAndUlong.UlongValue:X16}) we can show as double = ({doubleAndUlong.DoubleValue}).");
 
             Section("Enum in struct");
             var random = new Random();
@@ -135,6 +162,16 @@ namespace CodeSamples.Structures
             var enumInStruct = new EnumInStruct();
             enumInStruct.FileHeader = (uint)((attributes << 24) | (ownership << 16) | 0x5678);
             Console.WriteLine($"We have a FileHeader (in hex) = (0x{enumInStruct.FileHeader:X8}), we can extract Attributes = ({enumInStruct.Attributes.ToString("g")}) and ownership = ({enumInStruct.Ownership.ToString("g")}).");
+
+            Section("Boolean and bytes");
+            var boolToByte = new BoolToByte();
+            boolToByte.BoolProperty = false;
+            Console.WriteLine($"We have a Bool Property = {boolToByte.BoolProperty}, we can convert to byte = 0x{boolToByte.ByteProperty:X2}.");
+            boolToByte.BoolProperty = true;
+            Console.WriteLine($"We have a Bool Property = {boolToByte.BoolProperty}, we can convert to byte = 0x{boolToByte.ByteProperty:X2}.");
+
+            Section("Primitive types sizes");
+            PrimitiveTypesSizes();
 
             Finish();
         }
