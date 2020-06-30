024142e9c983b398a6dc2a72b7156de6972f06fd
Ales Hotko <ales.hotko@tompit.net>
2020-06-30 13:36:17 +0200

    Added ConfigurationManager Snippet

---
+++ CSharp Code Samples/CodeSamples/Settings/ConfigurationManagerSnippets.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Configuration;
+
+namespace CodeSamples.Settings
+{
+    internal class ConfigurationManagerSnippets
+    {
+        /// <summary>
+        /// Add following to App.config:
+        /// <example>
+        ///   <code>
+        /// =============================
+        ///     <appSettings>
+        ///       <add key="Setting1" value="May 5, 2014"/>
+        ///       <add key="Setting2" value="May 6, 2014"/>
+        ///     </appSettings>
+        /// =============================
+        ///   </code>
+        /// </example>
+        /// </summary>
+        private void ReadAllSettings()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                if (appSettings.Count == 0)
+                {
+                    Console.WriteLine("AppSettings is empty.");
+                }
+                else
+                {
+                    foreach (var key in appSettings.AllKeys)
+                    {
+                        Console.WriteLine("Key: {0} Value: {1}", key, appSettings[key]);
+                    }
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+        }
+
+        public void Run()
+        {
+            ReadAllSettings();
+        }
+    }
+}
--- CSharp Code Samples/CodeSamples/Settings/SettingsSample.cs
+++ CSharp Code Samples/CodeSamples/Settings/SettingsSample.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CodeSamples.Settings
 {
@@ -43,6 +39,9 @@ namespace CodeSamples.Settings
 
             LoadMainSettings();
             LoadOtherSettings();
+
+            var configurationManagerSnippets = new ConfigurationManagerSnippets();
+            configurationManagerSnippets.Run();
 
             Finish();
         }
